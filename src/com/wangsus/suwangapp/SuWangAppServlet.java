80489a820f79b56f8fe83b08489ab1d4dd7ae5f6
Su <wangsu@wangsus.com>
2014-01-09 12:10:29 -0500

    add user

--- src/com/wangsus/suwangapp/SuWangAppServlet.java
+++ src/com/wangsus/suwangapp/SuWangAppServlet.java
@@ -3,11 +3,29 @@ package com.wangsus.suwangapp;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class SuWangAppServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		UserService userService = UserServiceFactory.getUserService();
+
+        String thisURL = req.getRequestURI();
+
+        resp.setContentType("text/html");
+        if (req.getUserPrincipal() != null) {
+            resp.getWriter().println("<p>Hello, " +
+                                     req.getUserPrincipal().getName() +
+                                     "!  You can <a href=\"" +
+                                     userService.createLogoutURL(thisURL) +
+                                     "\">sign out</a>.</p>");
+        } else {
+            resp.getWriter().println("<p>Please <a href=\"" +
+                                     userService.createLoginURL(thisURL) +
+                                     "\">sign in</a>.</p>");
+        }
+
 	}
 }
