7ec68e52899afa2ffdab9ac075f657c03c1644f4
Su Wang <wangsu10@gmail.com>
2014-02-21 11:17:35 -0500

    Use jsp

--- src/com/wangsus/suwangapp/SuWangAppServlet.java
+++ src/com/wangsus/suwangapp/SuWangAppServlet.java
@@ -1,6 +1,8 @@
 package com.wangsus.suwangapp;
 
 import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import com.google.appengine.api.users.User;
@@ -10,13 +12,15 @@ import com.google.appengine.api.users.UserServiceFactory;
 @SuppressWarnings("serial")
 public class SuWangAppServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+			throws IOException, ServletException {
 		UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
 
 		if (user != null) {
-			resp.setContentType("text/plain");
-			resp.getWriter().println("Hello, " + user.getNickname());
+//			resp.setContentType("text/plain");
+//			resp.getWriter().println("Hello, " + user.getNickname());
+			req.setAttribute("name", user.getNickname());
+			req.getRequestDispatcher("/WEB-INF/jsp/test.jsp").forward(req, resp);			
 		} else {
 			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
 		}
